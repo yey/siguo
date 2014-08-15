4bba73e53c2da0f04324c758dc4c6e41eb36bb92
yey <Nicholas.lanke@gmail.com>
2014-08-15 17:43:52 +0800

    upload

--- model/Util.js
+++ model/Util.js
@@ -1,56 +1,35 @@
+var Util = exports;
+
 var http = require('http');
-var v = "9189";
-var waitTime = "95000";//ms
 var querystring = require('querystring');
-var data= querystring.stringify({Cookie:'_sid=12s25i4pkupnmmf4hc9u3sk3l4'});
+var fs = require('fs');
+var configPath = "../config/siguo.cfg";
+
 var opts = {
-	host: 's1.android.siguozhanji.muhenet.com',
-	port: 80,
-	path: '/boss.php?do=Fight&v='+v+'&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.4.1&pvb=2014-05-05 12:48:19',
-	method: 'POST',
-	timeout: 4000,
-    headers: {   
-            'Content-Type':"application/x-www-form-urlencoded",
-            "Content-Length": data.length  
-        }
-};
-
-function getTrade(opts){
-	var str = "";
-	var req = http.request(opts, function(res){
-		res.setEncoding('utf8');
-		res.on('end', function(data){
-            v++;
-			setTimeout(getTrade, waitTime, opts);
-		});
-	});
-    req.write(data);
-	req.end();
-}
-
-function buyGouLiang(startV,limit){
-    var data_buyGold = querystring.stringify({GoodsId:1});
-    var opts_buyGold = {
     host: 's1.android.siguozhanji.muhenet.com',
     port: 80,
-    path: '/shop.php?do=Buy&v='+startV+'&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.4.1&pvb=2014-05-05 12:48:19',
+    path: '&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.4.1&pvb=2014-05-05 12:48:19',
     method: 'POST',
     headers: {
             "Cookie":'_sid=12s25i4pkupnmmf4hc9u3sk3l4',
             "Connection": 'Keep-Alive',   
-            "Content-Type":"application/x-www-form-urlencoded",
-            "Content-Length": data_buyGold.length  
+            "Content-Type":"application/x-www-form-urlencoded" 
         }
     };
-console.log(opts_buyGold);
+//buy times = limit - startV
+Util.buyGouLiang = function(startV,limit){
+    var data_buyGold = querystring.stringify({GoodsId:1});
+    var opts_buyGold = opts;
+    opts_buyGold.path = '/shop.php?do=Buy&v='+startV+opts_buyGold.path;
+    opts_buyGold.headers["Content-Length"] = data_buyGold.length;
+
     var req = http.request(opts_buyGold, function(res){
         res.setEncoding('utf8');
         res.on('data', function(data){
             console.log(data);
             startV++;
             if(startV<limit){
-console.log(startV);
-                setTimeout(buyGouLiang, 1000, startV, limit);
+                setTimeout(Util.buyGouLiang, 1000, startV, limit);
             }
         });
     });
@@ -58,13 +37,33 @@ console.log(startV);
     req.end();
 }
 
-function init(){
-    //getTrade(opts);
-    var s = 2807;
-    console.log(s);
-    buyGouLiang(s, s+20);
+//fight boss
+Util.fightBoss = function(startV){
+    var data_fightBoss = querystring.stringify({GoodsId:1});
+    var opts_fightBoss = {
+    host: 's1.android.siguozhanji.muhenet.com',
+    port: 80,
+    path: '/boss.php?do=Fight&v='+v+'&phpp=ANDROID_XIAOMI&phpl=ZH_CN&pvc=1.4.1&pvb=2014-05-05 12:48:19',
+    method: 'POST',
+    headers: {
+            "Cookie":'_sid=12s25i4pkupnmmf4hc9u3sk3l4',
+            "Connection": 'Keep-Alive',   
+            "Content-Type":"application/x-www-form-urlencoded",
+            "Content-Length": data_fightBoss.length  
+        }
+    };
+    var waitTime = "95000";//ms
 }
+//狗粮购买成功，注意cookie的设置和data的传输。setTimeout的参数传递规则
 
-init();
-
-//狗粮购买成功，注意cookie的设置和data的传输。
+Util.setConfig = function(key, value){
+    var data = fs.readFileSync(configPath, 'utf-8');
+    var decodeData = JSON.parse(data);
+    decodeData[key] = value;
+    fs.writeFileSync(configPath, JSON.stringify(decodeData), ['utf8','w']);
+}
+Util.getConfig = function(key){
+    var data = fs.readFileSync(configPath, 'utf-8');
+    var decodeData = JSON.parse(data);
+    return decodeData[key];
+}
---
+++ test/testForClass.js
@@ -0,0 +1,13 @@
+var Unicode = require('../model/Unicode');
+var Util = require('../model/Util');
+
+// Util.setConfig("test", "fucker");
+// console.log(Util.getConfig("heh"));
+// console.log(Util.getConfig("test"));
+// console.log(Util.getConfig("v"));
+// Util.setConfig("v","asdfaskf");
+// console.log(Util.getConfig("v"));
+// console.log(Unicode.encode("这是一个测试"));
+// console.log(Unicode.decode("\u72c2\u6b22\u793c\u5305"));
+
+Util.buyGouLiang(2880, 2880+5);
