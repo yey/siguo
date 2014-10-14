20278a7538b656afcce448dbd194fc6387987cba
yey <Nicholas.lanke@gmail.com>
2014-10-14 22:05:49 +0800

    fcm:dev

--- test/dev.js
+++ test/dev.js
@@ -3,7 +3,11 @@ var Util = require('../model/Util');
 var later = require('later');
 
 function process(){
-	Util.planEnergy(5716);
+	//Util.buyGouLiang(5716,6016);
+	//Util.fightBossV2(5716);
+	//Util.planEnergy(5716);
+	Util.fightJJCV2(5716);
+	Util.getFEnergy(5716);
 }
 
 process();
